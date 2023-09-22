31c0f39df7f561a2de8789ead7ce7bd6b3bfa08b
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2023-09-22 09:13:48 +0200

    Ica alapja Létrehozva #1

---
+++ Ica.js
@@ -0,0 +1,17 @@
+import {Character} from './Character.js';
+export class Ica extends Character{
+    constructor(ctx, x, y, width, height){
+        const spriteSrc = './sprites/Ica_sprite.png';
+        let spriteWidth = 64;
+        let spriteHeight = 64;
+        const spriteAnimationFrames = {
+            run: {sXMax:9, sY:8},
+
+        };
+
+
+        super(ctx, x, y, width, height, spriteSrc, spriteWidth, spriteHeight, spriteAnimationFrames);
+    }
+
+
+}
