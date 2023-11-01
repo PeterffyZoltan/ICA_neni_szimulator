e379d04f0161196acccbe6ed6bc3a4a6e68d1fff
[Máté Varga] <[varga.mate.gellert@students.jedlik.eu]>
2023-11-01 13:56:36 +0100

    ICA néni dancing #3

--- startpage.js
+++ startpage.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     startButtonGlow();
-    
+    createCircularBackground();
+    dancingICA();
 });
 
 function startButtonGlow() {
@@ -8,3 +9,40 @@ function startButtonGlow() {
     startButton.classList.add("glow-animation");
 }
 
+function createCircularBackground() {
+    const background = document.getElementById("background");
+    const foodCircles = document.querySelectorAll(".food-circle");
+
+    const centerX = background.clientWidth / 2;
+    const centerY = background.clientHeight / 2;
+    const radius = background.clientWidth / 4;
+    const numCircles = foodCircles.length;
+
+    foodCircles.forEach((circle, index) => {
+        const angle = (index / numCircles) * 2 * Math.PI;
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
+
+        circle.style.display = "block";
+        circle.style.transform = `translate(${centerX + x}px, ${centerY + y}px)`;
+    });
+}
+
+function dancingICA() {    
+    const img = document.getElementById("ICAimg");
+    const sources = [
+        "assets/startpage/front.png", 
+        "assets/startpage/T.png",
+        "assets/startpage/right.png",
+        "assets/startpage/T.png",
+        "assets/startpage/left.png",
+        "assets/startpage/T.png",
+    ];
+
+    let index = 0;
+    function changeImageSource() {
+        img.src = sources[index];
+        index = (index + 1) % sources.length;
+    }
+    const interval = setInterval(changeImageSource, 500);
+}
