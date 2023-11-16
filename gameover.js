ed301db4b35147de1fad5b86ebbb57154d5fe50f
[Máté Varga] <[varga.mate.gellert@students.jedlik.eu]>
2023-11-16 10:33:00 +0100

    Dying ICA animation #12

--- gameover.js
+++ gameover.js
@@ -1,5 +1,32 @@
-const restart = document.getElementById('gameover').addEventListener('click', restartGame);
+window.addEventListener('load', () => {
+    dyingICA();
+});
+
+window.addEventListener('resize', () => {
+    clearInterval(interval);
+});
+
+const restart = document.getElementById('restart').addEventListener('click', restartGame);
 
 function restartGame() {
     window.open("./game.html", "_self");
+}
+
+function dyingICA() {    
+    const img = document.getElementById("ICAimg");
+    const sources = [
+        "assets/endpage/1.png",
+        "assets/endpage/2.png",
+        "assets/endpage/3.png",
+        "assets/endpage/4.png",
+        "assets/endpage/5.png",
+        "assets/endpage/6.png",
+    ];
+
+    let index = 0;
+    function changeImageSource() {
+        img.src = sources[index];
+        index = (index + 1) % sources.length;
+    }
+    const interval = setInterval(changeImageSource, 500);
 }
--- win.js
+++ win.js
@@ -1,4 +1,4 @@
-const restart = document.getElementById('win').addEventListener('click', restartGame);
+const restart = document.getElementById('restart').addEventListener('click', restartGame);
 
 function restartGame() {
     window.open("./game.html", "_self");
